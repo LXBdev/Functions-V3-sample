0acd662c98b4e282b2548c9dd649da670c4b009b
Alex <xy.hochdorf@gmx.de>
2020-06-21 17:56:49 +0200

    Use AppInsights directly

--- MyService.cs
+++ MyService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Build.Framework;
+﻿using Microsoft.ApplicationInsights;
+using Microsoft.Build.Framework;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -8,17 +9,20 @@ namespace Functions_V3_sample
 {
     public class MyService
     {
-        public MyService(ILogger<MyService> logger)
+        public MyService(ILogger<MyService> logger, TelemetryClient telemetry)
         {
             Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            Telemetry = telemetry ?? throw new ArgumentNullException(nameof(telemetry));
         }
 
         public void Foo()
         {
-            Logger.LogError("Foo!");
+            Logger.LogError("Foo");
+            Telemetry.TrackEvent("Bar");
         }
 
 
         public ILogger<MyService> Logger { get; }
+        public TelemetryClient Telemetry { get; }
     }
 }
